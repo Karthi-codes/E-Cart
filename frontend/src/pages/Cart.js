51bb20cd53e46dcc8d5535f040905b47ac76a9ba
Karthi-codes <karthikarthikeyan16401@gmail.com>
2025-05-20 15:34:23 +0530

    fragments12

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -7,11 +7,11 @@ export default function Cart({cartItem,setCartItem}){
    
    
     function increaseQty(item){
-        if(item.product.stock == item.qty){
+        if(item.product.stock === item.qty){
             return;
         }
         const updateitem= cartItem.map((i)=>{
-        if(i.product._id == item.product._id){
+        if(i.product._id === item.product._id){
             i.qty++;
         }
         return i;
@@ -22,7 +22,7 @@ export default function Cart({cartItem,setCartItem}){
     function decreaseQty(item){
         if(item.qty>1){
             const updateitem = cartItem.map((i)=>{
-                if(i.product._id == item.product._id){
+                if(i.product._id === item.product._id){
                     i.qty--;
                 }
                 return i;
